2d307a05a2480bb3321d6982e2525a34ea5c8cfa
wreed3rd <wreed3rd@gmail.com>
2022-02-21 21:50:07 -0500

    Chapter 13 studio upload

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,19 +1,33 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.*;
+
 public class User {
+
+    @NotEmpty(message="Username cannot be blank")
+    @Size(min=5, max=15)
     private String username;
+
+    @Email(message = "Please provide a valid email")
     private String email;
+
+    @Size(min=6)
+    @NotEmpty(message= "Password cannot be blank")
     private String password;
+
+    @NotNull(message= "Passwords do not match")
+    private String verifyPassword;
 
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String verifyPassword) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verifyPassword = verifyPassword;
     }
 
     public String getUsername() {
@@ -38,5 +52,23 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
     }
-}
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
+
+    }
+
+    private void checkPassword(){
+        if(!this.password.isEmpty() && (this.verifyPassword != null) && !this.password.equals(this.verifyPassword)){
+                this.verifyPassword = null;
+            }
+        }
+    }
+
